a1b4c063708397390059da9d02437185120acbcb
Sofía Maturana <sofimaturana830@gmail.com>
2026-01-13 08:55:45 -0300

    feat: Retocar detalles y agregar galería

--- _config.ts
+++ _config.ts
@@ -1,7 +1,14 @@
 import lume from "lume/mod.ts";
+import icons from "lume/plugins/icons.ts";
+import inline from "lume/plugins/inline.ts";
+import unocss from "lume/plugins/unocss.ts";
 
 const site = lume();
 
 site.add("/assets");
+
+site.use(unocss({ cssFile: "assets/css/uno.css" })).use(icons()).use(inline());
+
+site.add("unocss.css");
 
 export default site;
